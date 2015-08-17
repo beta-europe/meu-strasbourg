3bf306e9c5c9e9a8f24782fdf0d2fb7e9db51d3d
Robert Riemann <robert@riemann.cc>
2015-08-17 02:23:49 +0200

    extern material

--- Gemfile
+++ Gemfile
@@ -1,16 +1,20 @@
-ruby "2.1.5"
+ruby '2.1.5'
 
 # If you do not have OpenSSL installed, update
 # the following line to use "http://" instead
 source 'https://rubygems.org'
 
-gem "middleman", "~>3.4.0"
-
-# Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
-
 # For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
 
 # Windows does not come with time zone data
-gem "tzinfo-data", platforms: [:mswin, :mingw, :jruby]
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+
+gem 'middleman', :git => 'https://github.com/middleman/middleman.git', :branch => 'master'
+gem 'middleman-deploy', '~> 2.0.0-alpha'
+
+gem 'middleman-livereload'
+
+gem 'slim'
+
+gem 'material_design_lite-rails', require: false
--- config.rb
+++ config.rb
@@ -1,11 +1,13 @@
-###
-# Compass
-###
+set :css_dir,               'css'
+set :js_dir,                'js'
+set :images_dir,            'img'
+set :fonts_dir,             'fonts'
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+# Slim template engine
+require 'slim'
+
+# explicit require of sass as suggested by 'tilt'
+require 'sass'
 
 ###
 # Page options, layouts, aliases and proxies
@@ -14,31 +16,20 @@
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
 
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
 ###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -47,26 +38,24 @@
 #   end
 # end
 
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
+configure :development do
+  # Reload the browser automatically whenever files change
+  activate :livereload
+  
+  # Slim configuration
+  set :slim, {
+    format: :html,
+    indent: '    ',
+    pretty: true,
+    sort_attrs: false
+  }
+end
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
+  # Minify CSS on build
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  activate :minify_javascript
 end
